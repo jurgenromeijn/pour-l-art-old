63403b7b356a264cda3e50dc7ec4065f167eb6ba
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2014-11-24 20:51:44 +0100

    Gulp now deploys assets. Updated gulp asset paths.

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,46 @@ var gulp = require('gulp'),
 gulp.task('default', ['build-dev'], function() {
 });
 
-gulp.task('build-dev', function() {
+gulp.task('build', ['vendor', 'deploy-html', 'deploy-assets'], function() {
 	gulp.src('src/app/assets/scss/styles.scss')
 		.pipe(sass())
 		.pipe(autoprefixer())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('build/webroot/assets/css'));
+		.pipe(gulp.dest('build/webroot/css'));
+});
 
+gulp.task('build-dev', ['vendor', 'deploy-html', 'deploy-assets'], function() {
+	gulp.src('src/app/assets/scss/styles.scss')
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('build/webroot/css'));
+});
+
+gulp.task('deploy-html', function() {
 	gulp.src('src/app/html/*.html')
 		.pipe(gulp.dest('build/webroot'));
 });
 
-gulp.task('build-dev', ['vendor'], function() {
-	gulp.src('src/app/assets/scss/styles.scss')
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('build/webroot/assets/css'));
+gulp.task('deploy-assets', function() {
+	gulp.src('src/app/assets/images/**')
+		.pipe(gulp.dest('build/images'));
 
-	gulp.src('src/app/html/*.html')
-		.pipe(gulp.dest('build/webroot'));
+	gulp.src('src/app/assets/fonts/**')
+		.pipe(gulp.dest('build/fonts'));
+
+	gulp.src('src/app/assets/js/**')
+		.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('vendor', ['vendor-bootstrap']);
 
 gulp.task('vendor-bootstrap', function() {
 	gulp.src('vendor/bootstrap-sass-3.3.1/assets/fonts/**')
-		.pipe(gulp.dest('build/webroot/assets/fonts'));
+		.pipe(gulp.dest('build/webroot/fonts'));
 
 	gulp.src('vendor/bootstrap-sass-3.3.1/assets/images/**')
-		.pipe(gulp.dest('build/webroot/assets/images'));
+		.pipe(gulp.dest('build/webroot/images'));
 
 	gulp.src('vendor/bootstrap-sass-3.3.1/assets/javascripts/**')
-		.pipe(gulp.dest('build/webroot/assets/js'));
+		.pipe(gulp.dest('build/webroot/js'));
 });

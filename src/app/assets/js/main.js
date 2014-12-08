18058d8218334f6eb591034b3661ebc5cab14ad9
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2014-12-08 07:37:50 +0100

    Fixed hero part.

--- src/app/assets/js/main.js
+++ src/app/assets/js/main.js
@@ -48,5 +48,6 @@ var scrollLink = (function() {
 		var target = $(this).attr('href');
 		e.preventDefault();
 		$("html, body").animate({ scrollTop: $(target).offset().top }, 1000);
+		$(this).blur();
 	});
 })();
